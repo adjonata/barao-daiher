58cf1749a8d86f2a5cd1eca5378d837626a596b0
adjonata <alexdjonata30@gmail.com>
2021-04-03 15:49:11 -0300

    feat: container component

---
+++ src/components/Container.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="container">
+    <div class="container-content">
+      <h3 class="container-title" v-if="title">{{ title }}</h3>
+      <slot />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Container',
+  props: {
+    title: String,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  padding: 40px 20px;
+  display: flex;
+  justify-content: center;
+
+  @media (max-width: $mobile) {
+    padding: 15px;
+  }
+
+  &-content {
+    width: 100%;
+    max-width: 1344px;
+    text-align: center;
+  }
+
+  &-title {
+    width: 100%;
+    font-size: 21px;
+    color: $dark;
+    margin-bottom: 40px;
+    text-transform: uppercase;
+
+    @media (max-width: $mobile) {
+      font-size: 17px;
+      margin-bottom: 20px;
+    }
+
+    &::before {
+      content: '----';
+      letter-spacing: -2px;
+      margin-right: 12px;
+      color: $grey;
+    }
+    &::after {
+      content: '----';
+      letter-spacing: -2px;
+      margin-left: 12px;
+      color: $grey;
+    }
+  }
+}
+</style>
