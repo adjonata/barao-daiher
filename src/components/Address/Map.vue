f68636b7d02ba68219df524400f92d1e7af7684b
adjonata <alexdjonata30@gmail.com>
2021-04-03 15:50:31 -0300

    feat: address section

---
+++ src/components/Address/Map.vue
@@ -0,0 +1,25 @@
+<template>
+  <iframe
+    :src="source"
+    width="600"
+    height="450"
+    frameborder="0"
+    style="border: 0"
+    allowfullscreen=""
+    aria-hidden="false"
+    tabindex="0"
+  />
+</template>
+
+<script>
+import { source } from '@/utils/address'
+
+export default {
+  name: 'Map',
+  data: () => ({
+    source,
+  }),
+}
+</script>
+
+<style lang="scss" scoped></style>
