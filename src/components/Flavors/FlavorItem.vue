9f8d7fc2ef0cc8eab0c75a5da669e4a57821a292
adjonata <alexdjonata30@gmail.com>
2021-04-03 01:31:55 -0300

    feat: Flavors components

---
+++ src/components/Flavors/FlavorItem.vue
@@ -0,0 +1,68 @@
+<template>
+  <article class="flavor-item">
+    <img :src="image" alt="Imagem do produto.jpeg" class="flavor-item__image" />
+    <h3 class="flavor-item__title">{{ title }}</h3>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'FlavorItem',
+  props: {
+    /**
+     * Imagem do produto
+     */
+    image: {
+      type: String,
+      required: true,
+    },
+    /**
+     * Título do produto
+     */
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.flavor-item {
+  background: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 15px 35px 25px;
+  -webkit-box-shadow: -1px 5px 8px 4px rgba(0, 0, 0, 0.24);
+  box-shadow: -1px 5px 8px 4px rgba(0, 0, 0, 0.24);
+  border-radius: 10px;
+
+  @media (max-width: $mobile) {
+    margin: 15px 10px;
+  }
+
+  &__image {
+    width: 170px;
+    height: 281.33px;
+    border-radius: 10px 10px 0 0;
+    filter: contrast(110%);
+
+    @media (max-width: $mobile) {
+      width: 140px;
+      height: auto;
+    }
+  }
+
+  &__title {
+    width: 100%;
+    line-height: 40px;
+    font-weight: lighter;
+    font-size: 15px;
+    font-weight: 600;
+    background: $burned-yellow-2;
+    color: #f1f1f1;
+    border-radius: 0 0 10px 10px;
+  }
+}
+</style>
---
+++ src/components/Flavors/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="section flavors" id="sabores">
+    <div class="section-content">
+      <h2 class="section-title">Sabores</h2>
+      <div class="flavors-list">
+        <FlavorItem
+          v-for="(flavor, index) in flavors"
+          :key="'flavor_' + index"
+          :image="flavor.image"
+          :title="flavor.title"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import flavors from '@/utils/flavors'
+import FlavorItem from './FlavorItem'
+
+export default {
+  name: 'Flavors',
+  components: { FlavorItem },
+  data: () => ({
+    flavors,
+  }),
+}
+</script>
+
+<style lang="scss" scoped>
+.flavors {
+  margin-top: 50px;
+
+  @media (max-width: $mobile) {
+    margin-top: 20px;
+  }
+
+  &-list {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+
+    @media (max-width: $mobile) {
+      overflow-x: auto;
+      flex-wrap: nowrap;
+      justify-content: flex-start;
+    }
+  }
+}
+</style>
