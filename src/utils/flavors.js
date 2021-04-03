a2d96ba3d82da40f624cc03efaccdc7c15756c22
adjonata <alexdjonata30@gmail.com>
2021-04-03 01:31:31 -0300

    feat: utils

---
+++ src/utils/flavors.js
@@ -0,0 +1,53 @@
+import Banana from '@/assets/banana.jpeg'
+import Cafe from '@/assets/cafe.jpeg'
+import CanelaECravo from '@/assets/canela-e-cravo.jpeg'
+import Coco from '@/assets/coco.jpeg'
+import GengibreECravo from '@/assets/gengibre-e-cravo.jpeg'
+import MacaVerde from '@/assets/maca-verde.jpeg'
+import Milho from '@/assets/milho.jpeg'
+import Ouro from '@/assets/ouro.jpeg'
+import Prata from '@/assets/prata.jpeg'
+import Umburana from '@/assets/umburana.jpeg'
+
+export default [
+  {
+    title: 'Banana',
+    image: Banana,
+  },
+  {
+    title: 'Café',
+    image: Cafe,
+  },
+  {
+    title: 'Canela e Cravo',
+    image: CanelaECravo,
+  },
+  {
+    title: 'Coco',
+    image: Coco,
+  },
+  {
+    title: 'Gengibre e Cravo',
+    image: GengibreECravo,
+  },
+  {
+    title: 'Maça Verde',
+    image: MacaVerde,
+  },
+  {
+    title: 'Milho',
+    image: Milho,
+  },
+  {
+    title: 'Ouro',
+    image: Ouro,
+  },
+  {
+    title: 'Prata',
+    image: Prata,
+  },
+  {
+    title: 'Umburana',
+    image: Umburana,
+  },
+]
---
+++ src/utils/sections.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    to: '#sabores',
+    title: 'Sabores',
+  },
+  {
+    to: '#',
+    title: 'Endereço',
+  },
+  {
+    to: '#',
+    title: 'Contato',
+  },
+]
