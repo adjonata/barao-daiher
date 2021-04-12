2083cb8f305c432f618be883744ab67be570600e
adjonata <alexdjonata30@gmail.com>
2021-04-12 19:21:21 -0300

    feat: Footer in App.vue

--- src/components/index.js
+++ src/components/index.js
@@ -4,8 +4,10 @@ import Container from './Container'
 import Address from './Address'
 import Flavors from './Flavors'
 import Hero from './Hero'
+import Footer from './Footer'
 
 Vue.component('Container', Container)
 Vue.component('Address', Address)
 Vue.component('Flavors', Flavors)
 Vue.component('Hero', Hero)
+Vue.component('Footer', Footer)
